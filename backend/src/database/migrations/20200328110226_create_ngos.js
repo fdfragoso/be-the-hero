92a5612bc4757c3826f4c19ed4c831b97fb2e297
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-03-28 11:57:06 +0100

    Create NGO migration

---
+++ backend/src/database/migrations/20200328110226_create_ngos.js
@@ -0,0 +1,17 @@
+
+//execute the migration 
+exports.up = function(knex) {
+  return knex.schema.createTable('ngos', function (table) {
+    table.string('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('whatsapp').notNullable();
+    table.string('city').notNullable();
+    table.string('uf', 2).notNullable();
+  });
+};
+
+//if need roolback and change stuff
+exports.down = function(knex) {
+  return knex.schema.dropTable('ngos');
+};
