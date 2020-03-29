99184228572ab73bd69ae419da3293608b64c297
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-03-29 18:23:05 +0200

    add routes page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './global.css';
-import Logon from './pages/Logon';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <Logon />
+    <Routes />
   );
 }
 
