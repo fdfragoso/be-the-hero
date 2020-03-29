c70f536a68802a33a61cb70e1695425d969f164e
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-03-29 19:49:08 +0200

    add initial structure to profile index.html

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link }from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be the Hero"/>
+                <span>Welcome, Red Cross</span>
+
+                <Link className="button" to="/cases/new">Register new case</Link>
+                <button type="button">
+                    <FiPower size={18} color="#E02041" />
+                </button>
+            </header>
+        </div>
+    );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Logon} />
-                <Route path="/register" component={Register } />
+                <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
             </Switch>
         </BrowserRouter>
     );
