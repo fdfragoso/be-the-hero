857c65feea722b8d4e1f47fac9f3924dcbe6486c
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-03-29 19:19:53 +0200

    add index register page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
@@ -18,10 +19,10 @@ export default function Logon() {
                     <input placeholder="Your ID" />
                     <button className="button" type="submit">Enter</button>
 
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Sign up
-                    </a>
+                    </Link>
                 </form>
             </section>
 
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
+import logoImg from '../../assets/logo.svg';
+
 export default function Register() {
-    return <h1>Register</h1> 
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be the hero"/>
+
+                    <h1>Sign up</h1>
+                    <p>
+                        Sign up and help people find cases from NGOs.
+                    </p>
+
+                    <Link className="back-link" to="/register">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Sign up
+                    </Link>
+                </section>
+
+                <form>
+                    <input placeholder="NGO Name" />
+                    <input type="email" placeholder="Email" />
+                    <input placeholder="Whatsapp" />
+
+                    <div className="input-group">
+                        <input placeholder="City" />
+                        <input placeholder="UF" style={{ width: 80 }} />
+                    </div>
+
+                    <button className="button" type="submit">Sign Up</button>
+                </form>
+            </div>
+        </div>
+    );
 }
