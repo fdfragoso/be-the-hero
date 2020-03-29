e1ac32a24287dcf7e6a5f760f55c8eb813562eae
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-03-29 19:55:25 +0200

    add profile header style

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link }from 'react-router-dom';
 import { FiPower } from 'react-icons/fi';
 
+import './styles.css';
+
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
